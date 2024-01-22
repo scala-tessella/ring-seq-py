3643657b4a28c99b216a76910ffa9b752483a626
Mario Càllisto <mario.callisto@gmail.com>
2024-01-22 20:23:24 +0100

    Functions reST documented

--- src/ring_seq/RingSeq.py
+++ src/ring_seq/RingSeq.py
@@ -5,6 +5,7 @@ class RingSeq:
     def __init__(self, underlying: Seq):
         """
 
+        :param underlying: sequence considered circular
         :type underlying: Seq
         """
         self.underlying = underlying
@@ -12,98 +13,167 @@ class RingSeq:
     def index_from(self, i: IndexO) -> Index:
         """
 
-        :type i: IndexO
+        :param i: circular index
+        :type i: Index
+        :return: standard index
+        :rtype: Any
         """
         return index_from(self.underlying, i)
 
     def apply_o(self, i: IndexO) -> Any:
         """
 
+        :param i: circular index
         :type i: IndexO
+        :return: element at circular index
+        :rtype: Any
         """
         return apply_o(self.underlying, i)
 
     def rotate_right(self, step: IndexO) -> Seq:
         """
 
+        :param step: number of rotation steps to the right
         :type step: IndexO
+        :return: rotated sequence
+        :rtype: Seq
         """
         return rotate_right(self.underlying, step)
 
     def rotate_left(self, step: IndexO) -> Seq:
         """
 
+        :param step: number of rotation steps to the left
         :type step: IndexO
+        :return: rotated sequence
+        :rtype: Seq
         """
         return rotate_left(self.underlying, step)
 
     def start_at(self, i: IndexO) -> Seq:
         """
 
+        :param i: circular index where the sequence starts
         :type i: IndexO
+        :return: rotated sequence
+        :rtype: Seq
         """
         return start_at(self.underlying, i)
 
     def reflect_at(self, i: IndexO = 0) -> Seq:
         """
 
+        :param i: circular index where the reflected sequence starts
         :type i: IndexO, default = 0
+        :return: reflected sequence
+        :rtype: Seq
         """
         return reflect_at(self.underlying, i)
 
     def slice_o(self, frm: IndexO, to: IndexO) -> Seq:
         """
 
+        :param frm: circular index where the slice starts
         :type frm: IndexO
+        :param to: circular index where the slice ends
         :type to: IndexO
+        :return: sliced sequence
+        :rtype: Seq
         """
         return slice_o(self.underlying, frm, to)
 
     def rotations(self) -> Iterator[Seq]:
+        """
+
+        :return: sequence and its rotations, 1 step at a time to the right
+        :rtype: Iterator[Seq]
+        """
         return rotations(self.underlying)
 
     def reflections(self) -> Iterator[Seq]:
+        """
+
+        :return: sequence and its reflection
+        :rtype: Iterator[Seq]
+        """
         return reflections(self.underlying)
 
     def reversions(self) -> Iterator[Seq]:
+        """
+
+        :return: sequence and its reversion
+        :rtype: Iterator[Seq]
+        """
         return reversions(self.underlying)
 
     def rotations_and_reflections(self) -> Iterator[Seq]:
+        """
+
+        :return: sequence and its rotations, and their reflections
+        :rtype: Iterator[Seq]
+        """
         return rotations_and_reflections(self.underlying)
 
     def is_rotation_of(self, that: Seq) -> bool:
         """
 
+        :param that: sequence to be compared
         :type that: Seq
+        :return: true if equal to any rotation of that
+        :rtype: bool
         """
         return is_rotation_of(self.underlying, that)
 
     def is_reflection_of(self, that: Seq) -> bool:
         """
 
+        :param that: sequence to be compared
         :type that: Seq
+        :return: true if equal to any reflection of that
+        :rtype: bool
         """
         return is_reflection_of(self.underlying, that)
 
     def is_reversion_of(self, that: Seq) -> bool:
         """
 
+        :param that: sequence to be compared
         :type that: Seq
+        :return: true if equal to any reversion of that
+        :rtype: bool
         """
         return is_reversion_of(self.underlying, that)
 
     def is_rotation_or_reflection_of(self, that: Seq) -> bool:
         """
 
+        :param that: sequence to be compared
         :type that: Seq
+        :return: true if equal to any combination of rotation and reversion of that
+        :rtype: bool
         """
         return is_rotation_or_reflection_of(self.underlying, that)
 
     def rotational_symmetry(self) -> int:
+        """
+
+        :return: rotational symmetry value
+        :rtype: int
+        """
         return rotational_symmetry(self.underlying)
 
     def symmetry_indices(self) -> list[Index]:
+        """
+
+        :return: indices of the elements by which the reflectional symmetry axis are near
+        :rtype: list[Index]
+        """
         return symmetry_indices(self.underlying)
 
     def symmetry(self) -> int:
+        """
+
+        :return: reflectional symmetry value
+        :rtype: int
+        """
         return symmetry(self.underlying)
