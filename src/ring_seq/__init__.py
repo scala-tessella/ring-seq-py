99d93c5667996ab652fb1c0c1c7d1ddd23339822
Mario Càllisto <mario.callisto@gmail.com>
2024-01-23 17:32:46 +0100

    Docstring for packages

--- src/ring_seq/__init__.py
+++ src/ring_seq/__init__.py
@@ -1,1 +1,12 @@
+"""Extends `list`, `tuple` and `str` with ring (circular) methods.
+
+Add new operations to `list`, `tuple` and `str`
+for when such a sequence needs to be considered **circular**,
+its elements forming a ring.
+
+Modules exported by this package:
+
+- `methods`: Contains all the library methods plus new types.
+- `RingSeq`: Contains the `RingSeq` class.
+"""
 from ring_seq.RingSeq import RingSeq
---
+++ src/ring_seq/examples/__init__.py
@@ -0,0 +1,6 @@
+"""Examples for the library.
+
+Modules exported by this package:
+
+- `Ring`: Contains the example `Ring` class.
+"""
