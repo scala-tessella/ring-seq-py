345765bba5314eead75554f7098f152c289b4116
Mario Càllisto <mario.callisto@gmail.com>
2024-01-24 15:25:26 +0100

    More iterating test

--- tests/IteratingTest.py
+++ tests/IteratingTest.py
@@ -36,6 +36,13 @@ class IteratingOps(unittest.TestCase):
                 "AEDCB", "EDCBA", "DCBAE", "CBAED", "BAEDC"
             ]
         )
+        self.assertEqual(
+            list(rotations_and_reflections(("A", 1, 'B', 2))),
+            [
+                ('A', 1, 'B', 2), (1, 'B', 2, 'A'),  ('B', 2, 'A', 1),  (2, 'A', 1, 'B'),
+                ('A', 2, 'B', 1), (2, 'B', 1, 'A'),  ('B', 1, 'A', 2),  (1, 'A', 2, 'B')
+            ]
+        )
 
 
 if __name__ == '__main__':
